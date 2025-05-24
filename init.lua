a7102d5f9b6f5621106bf5bb5bec1f843d5ff940
Nick Brown <nickbrown.dev@gmail.com>
2025-05-25 00:13:34 +0100

    Added initial proxy setup

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
+local hostname = vim.uv.os_gethostname()
+
+if (hostname == "abc123") then
+    vim.g.proxy_required = true
+    vim.g.proxy = "my proxy here"
+else
+    vim.g.proxy_required = false
+end
+
 require "keymaps"
 require "settings"
 require "autocommands"
