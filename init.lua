7282a6c2d3c4cefe0b31d57aa8d196cce50daece
Nick Brown <nicolas.w.brown@global.lmco.com>
2025-06-05 15:59:08 +0100

    Update workspace description

--- init.lua
+++ init.lua
@@ -6,14 +6,14 @@ if (hostname == "UK05CG2089J5Y") then -- Work laptop
     vim.g.proxy_required = false
     -- vim.g.proxy = "proxy-zs3.global.lmco.com:80"
     local repoDir = "C:\\Users\\e314680\\repo\\james"
-    workspaces["JAMES"] = {name = "JAMES", key = "j", location = repoDir .. "\\james_dev"}
+    workspaces["JAMES"] = {name = "JAMES root", key = "j", location = repoDir .. "\\james_dev"}
     workspaces["JAMES App"] = {name = "JAMES App", key = "a", location = repoDir .. "\\james_dev\\Application"}
     workspaces["DMI"] = {name = "DMI", key = "d", location = repoDir .. "\\james_dev\\Deployed_Mgmt_Interface\\Application"}
 elseif (hostname == "WAROWDA-TSS01") then -- Work VM
     vim.g.proxy_required = true
     vim.g.proxy = "http://proxy-zs3.global.lmco.com:80"
     local repoDir = "C:\\Users\\nick\\repo\\james"
-    workspaces["JAMES"] = {name = "JAMES", key = "j", location = repoDir .. "\\james_dev"}
+    workspaces["JAMES"] = {name = "JAMES root", key = "j", location = repoDir .. "\\james_dev"}
     workspaces["JAMES App"] = {name = "JAMES App", key = "a", location = repoDir .. "\\james_dev\\Application"}
     workspaces["DMI"] = {name = "DMI", key = "d", location = repoDir .. "\\james_dev\\Deployed_Mgmt_Interface\\Application"}
 else
