d7a97df3d92abf62278aa85dbf449a0daa4ab84b
Nick Brown <nicolas.w.brown@global.lmco.com>
2025-06-05 15:29:20 +0100

    Added workspace folders for quick switching

--- init.lua
+++ init.lua
@@ -1,18 +1,25 @@
 local hostname = vim.uv.os_gethostname()
 
-if (hostname == "abc123") then
-    vim.g.proxy_required = true
-    vim.g.proxy = "my proxy here"
+local workspaces = {}
+if (hostname == "UK05CG2089J5Y") then -- Work laptop
+    vim.g.proxy_required = false
+    -- vim.g.proxy = "proxy-zs3.global.lmco.com:80"
+    local repoDir = "C:\\Users\\e314680\\repo\\james"
+    workspaces["JAMES"] = {name = "JAMES", key = "j", location = repoDir .. "\\james_dev"}
+    workspaces["JAMES App"] = {name = "JAMES App", key = "a", location = repoDir .. "\\james_dev\\Application"}
+    workspaces["DMI"] = {name = "DMI", key = "d", location = repoDir .. "\\james_dev\\Deployed_Mgmt_Interface\\Application"}
 else
     vim.g.proxy_required = false
 end
+
+vim.g.workspaces = workspaces
 
 require "keymaps"
 require "settings"
 require "autocommands"
 if vim.g.neovide then
     -- List of alternate GUIs: https://github.com/neovim/neovim/wiki/Related-projects#gui
-    require('neovide')
+    require 'neovide'
 end
-require "lsp"
+-- require "lsp"
 require "plugins"
